968703e81ca7aed13bb29c78e002e8bf055ff890
RuslanaLos <ruslanalos74@gmail.com>
2023-08-22 12:40:35 +0300

    hw_12

---
+++ js/test1.js
@@ -0,0 +1,13 @@
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
+
+function removeElement(array, item){
+    let index = array.indexOf(item);
+    if (index !== -1){
+        array.splice(index, 1);
+    }
+}
+
+console.log(array);
+
+
